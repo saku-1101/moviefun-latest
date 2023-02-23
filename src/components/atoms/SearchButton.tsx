6ac9a4afcf7232c7000f96c05be4748058485c42
saku-1101 <sakupi1101@outlook.jp>
2023-02-23 17:06:18 +1100

    [add]ルーター追加

--- src/components/atoms/SearchButton.tsx
+++ src/components/atoms/SearchButton.tsx
@@ -1,9 +1,12 @@
 import { AiOutlineSearch } from 'react-icons/ai';
 import Button from 'react-bootstrap/Button';
 
+// import Search from '~/organisms/Search';
+// import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
 export default function SearchButton() {
   return (
-    <Button>
+    <Button href="/search">
       <AiOutlineSearch />
     </Button>
   );
