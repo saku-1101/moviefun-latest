62972acd223b487633fe1f0c220485c27720fe7b
saku-1101 <sakupi1101@outlook.jp>
2023-02-23 14:18:07 +1100

    [add]react-anchor-link-smooth-scrollページ内リンクのため

---
+++ src/components/atoms/SearchButton.tsx
@@ -0,0 +1,10 @@
+import { AiOutlineSearch } from 'react-icons/ai';
+import Button from 'react-bootstrap/Button';
+
+export default function SearchButton() {
+  return (
+    <Button>
+      <AiOutlineSearch />
+    </Button>
+  );
+}
