fed6d4d16431788f44ad4359ecbfa18b9301dc74
saku-1101 <sakupi1101@outlook.jp>
2023-02-23 14:08:46 +1100

    [add]url props 追加

--- src/components/atoms/ButtonInCard.tsx
+++ src/components/atoms/ButtonInCard.tsx
@@ -1,5 +1,9 @@
 import Button from 'react-bootstrap/Button';
 
-export default function ButtonIconCard() {
-  return <Button>VISIT</Button>;
+export default function ButtonIconCard(movie_url: string) {
+  return (
+    <Button href={movie_url} target="_blank">
+      VISIT
+    </Button>
+  );
 }
