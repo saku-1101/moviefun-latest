3e6c2000f448e92d8284a636e27c202313bdff51
saku-1101 <sakupi1101@outlook.jp>
2023-02-23 17:17:49 +1100

    [update]ルーティング装飾

--- src/components/atoms/SearchButton.tsx
+++ src/components/atoms/SearchButton.tsx
@@ -1,13 +1,14 @@
 import { AiOutlineSearch } from 'react-icons/ai';
 import Button from 'react-bootstrap/Button';
-
-// import Search from '~/organisms/Search';
-// import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 export default function SearchButton() {
   return (
-    <Button href="/search">
-      <AiOutlineSearch />
-    </Button>
+    // <a>やhrefなどでページ遷移を指定するとリロードが発生する
+    <NavLink style={({ isActive }) => (isActive ? { color: 'pink' } : undefined)} to="/search" target="_blank">
+      <Button>
+        <AiOutlineSearch />
+      </Button>
+    </NavLink>
   );
 }
