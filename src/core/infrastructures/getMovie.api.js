f23a1023653b48861dfca4d081f359891880f588
saku-1101 <sakupi1101@outlook.jp>
2023-02-27 10:47:49 +1100

    [add]getIDofMovies API tempraly

--- src/core/infrastructures/getMovie.api.js
+++ src/core/infrastructures/getMovie.api.js
@@ -9,9 +9,9 @@ var axiosInstance = axios_1["default"].create({
         'x-api-key': node_process_1.env.API_KEY
     }
 });
-console.log(node_process_1.env.API_KEY);
 var responseBody = function (res) {
     console.log(res);
+    return res.data;
 };
 function getTheatreMovies() {
     var date = new Date();
@@ -67,4 +67,39 @@ function getTopOfYear() {
         console.log(err);
     });
 }
-getTopOfYear();
+function searchMovie(input) {
+    var endpoint = '/search/movie';
+    var language = '&language=ja-JA';
+    var url = endpoint + '?api_key=' + node_process_1.env.API_KEY + '&query=' + input + language;
+    return axiosInstance
+        .get(url)
+        .then(responseBody)["catch"](function (err) {
+        console.log(err);
+    });
+    // 多分const urlでも大丈夫．．．
+    // UIにインプットしてる時→関数は発火されていない
+    // 関数発火時→新たなセッション．関数もなんでも一新される？
+    // もし，urlにconstが使えなかったら変数をgetに直入れ
+}
+// eslint-disable-next-line @typescript-eslint/no-unused-vars, @typescript-eslint/no-empty-function
+// function getFavoriteMovies(params:type) {
+// }
+function getIDofMovies() {
+    var endpoint = '/genre/movie/list';
+    var language = '&language=ja-JA';
+    var url = endpoint + '?api_key=' + node_process_1.env.API_KEY + language;
+    return axiosInstance
+        .get(url)
+        .then(responseBody)["catch"](function (error) {
+        if (error.response) {
+            console.error("Error: ".concat(error.response.status, " ").concat(error.response.statusText));
+        }
+        else if (error.request) {
+            console.error('No response received');
+        }
+        else {
+            console.error("Error: ".concat(error.message));
+        }
+    });
+}
+console.log(getIDofMovies());
