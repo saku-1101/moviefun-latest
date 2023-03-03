be16cc2c5aef5b25ed21275c65d76db0b8d42213
saku-1101 <sakupi1101@outlook.jp>
2023-03-03 23:43:34 +1100

    [add]redux setup

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,5 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
+import InputSliceReducer from '../features/InputStore/InputSlice';
 
 export default configureStore({
-  reducer: {},
+  reducer: {
+    inputStore: InputSliceReducer,
+  },
 });
+
+// Infer the `RootState` and `AppDispatch` types from the store itself
+export type RootState = ReturnType<typeof store.getState>;
+// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
+export type AppDispatch = typeof store.dispatch;
