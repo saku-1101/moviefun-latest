f3be1b12472870d24fc47c865bf292fc88d88180
saku-1101 <sakupi1101@outlook.jp>
2023-03-03 12:09:40 +1100

    [add]InputField for keyword search

---
+++ src/components/atoms/InputField.tsx
@@ -0,0 +1,20 @@
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import InputGroup from 'react-bootstrap/InputGroup';
+
+export default function InputField() {
+  return (
+    <>
+      <InputGroup className="mb-3">
+        <Form.Control
+          placeholder="Recipient's username"
+          aria-label="Recipient's username"
+          aria-describedby="basic-addon2"
+        />
+        <Button variant="outline-secondary" id="button-addon2">
+          Button
+        </Button>
+      </InputGroup>
+    </>
+  );
+}
