02a4cf4c09e5228baf16adc0deecfb583a3aa5d0
saku-1101 <sakupi1101@outlook.jp>
2023-03-05 08:18:59 +1100

    [add]movie recommendation router

--- src/components/atoms/InputField.tsx
+++ src/components/atoms/InputField.tsx
@@ -1,20 +1,33 @@
+// import getAllKeywordsIds from '../../core/infrastructures/getKeyword';
+import getIDofMovies from '../../core/infrastructures/getMovie';
+import { getPrefectures } from '../../core/infrastructures/api/Resas.api';
+import { useRef, useState } from 'react';
 import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form';
 import InputGroup from 'react-bootstrap/InputGroup';
 
 export default function InputField() {
+  const inputRef = useRef('');
+  // const [keys, getAllKeywordsIdsState] = useState([0]);
+  // const HandleGetAllKeywordsIds = async () => {
+  //   getAllKeywordsIdsState(await getAllKeywordsIds(inputRef.current.valueOf()));
+  // };
+  getIDofMovies();
+  // getAllKeywordsIds('sakura');
+  getPrefectures();
   return (
-    <>
+    <div>
       <InputGroup className="mb-3">
-        <Form.Control
-          placeholder="Recipient's username"
-          aria-label="Recipient's username"
-          aria-describedby="basic-addon2"
+        <input
+          ref={inputRef}
+          type="text"
+          id="message"
+          name="message"
+          placeholder="I wanna be fulfilled and confident."
         />
         <Button variant="outline-secondary" id="button-addon2">
           Button
         </Button>
       </InputGroup>
-    </>
+    </div>
   );
 }
