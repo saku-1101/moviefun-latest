a0e6ee4529c8d2ff72d5d75212c26e677398271c
saku-1101 <sakupi1101@outlook.jp>
2023-03-10 23:16:06 +1100

    [refactor]ファイル構造変更：dockerネットワーク形成のため

---
+++ client/src/app/store.ts
@@ -0,0 +1,21 @@
+export {};
+// import { configureStore } from '@reduxjs/toolkit';
+// import { applyMiddleware } from 'redux';
+// import thunkMiddleware from 'redux-thunk';
+// import InputSliceReducer from '../reducers/InputStore/InputSlice';
+// import { composeWithDevTools } from '@redux-devtools/extension';
+
+// // const composedEnhancer = composeWithDevTools(applyMiddleware(thunkMiddleware));
+
+// export default configureStore({
+//   reducer: {
+//     inputStore: InputSliceReducer,
+//   },
+//   middleware: [thunkMiddleware],
+//   devTools: true,
+// });
+
+// // Infer the `RootState` and `AppDispatch` types from the store itself
+// export type RootState = ReturnType<typeof store.getState>;
+// // Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
+// export type AppDispatch = typeof store.dispatch;
